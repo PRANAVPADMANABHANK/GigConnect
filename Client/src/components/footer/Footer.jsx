cac3550af582c9be8cdd44383026604cd08a3feb
pranav k <pranavkkeloth@gmail.com>
2023-07-19 13:32:42 +0530

    feat: Implement footer component

--- Client/src/components/footer/Footer.jsx
+++ Client/src/components/footer/Footer.jsx
@@ -1,10 +1,110 @@
-import React from 'react'
-import './Footer.scss'
+import React from "react";
+import "./Footer.scss";
 
 const Footer = () => {
   return (
-    <div className='footer'>Footer</div>
-  )
-}
+    <div className="footer">
+      <div className="container">
+        <div className="top">
+          <div className="item">
+            <h2>Categories</h2>
+            <span>Graphics & Design</span>
+            <span>Digital Marketing</span>
+            <span>Writing & Translation</span>
+            <span>video & Animation</span>
+            <span>Music & Audio</span>
+            <span>Programming & Tech</span>
+            <span>Data</span>
+            <span>Business</span>
+            <span>Lifestyle</span>
+            <span>Photography</span>
+            <span>Sitemap</span>
+          </div>
+          <div className="item">
+            <h2>Categories</h2>
+            <span>Graphics & Design</span>
+            <span>Digital Marketing</span>
+            <span>Writing & Translation</span>
+            <span>video & Animation</span>
+            <span>Music & Audio</span>
+            <span>Programming & Tech</span>
+            <span>Data</span>
+            <span>Business</span>
+            <span>Lifestyle</span>
+            <span>Photography</span>
+            <span>Sitemap</span>
+          </div>
+          <div className="item">
+            <h2>Categories</h2>
+            <span>Graphics & Design</span>
+            <span>Digital Marketing</span>
+            <span>Writing & Translation</span>
+            <span>video & Animation</span>
+            <span>Music & Audio</span>
+            <span>Programming & Tech</span>
+            <span>Data</span>
+            <span>Business</span>
+            <span>Lifestyle</span>
+            <span>Photography</span>
+            <span>Sitemap</span>
+          </div>
+          <div className="item">
+            <h2>Categories</h2>
+            <span>Graphics & Design</span>
+            <span>Digital Marketing</span>
+            <span>Writing & Translation</span>
+            <span>video & Animation</span>
+            <span>Music & Audio</span>
+            <span>Programming & Tech</span>
+            <span>Data</span>
+            <span>Business</span>
+            <span>Lifestyle</span>
+            <span>Photography</span>
+            <span>Sitemap</span>
+          </div>
+          <div className="item">
+            <h2>Categories</h2>
+            <span>Graphics & Design</span>
+            <span>Digital Marketing</span>
+            <span>Writing & Translation</span>
+            <span>video & Animation</span>
+            <span>Music & Audio</span>
+            <span>Programming & Tech</span>
+            <span>Data</span>
+            <span>Business</span>
+            <span>Lifestyle</span>
+            <span>Photography</span>
+            <span>Sitemap</span>
+          </div>
+        </div>
+        <hr />
+        <div className="bottom">
+          <div className="left">
+            <h2>GigConnect</h2>
+            <span>© Fiverr International Ltd. 2023</span>
+          </div>
+          <div className="right">
+            <div className="social">
+              <img src="../../../public/img/twitter.png" alt="" />
+              <img src="../../../public/img/facebook.png" alt="" />
+              <img src="../../../public/img/linkedin.png" alt="" />
+              <img src="../../../public/img/pinterest.png" alt="" />
+              <img src="../../../public/img/instagram.png" alt="" />
+            </div>
+            <div className="link">
+              <img src="../../../public/img/language.png" alt="" />
+              <span>English</span>
+            </div>
+            <div className="link">
+              <img src="../../../public/img/coin.png" alt="" />
+              <span>USD</span>
+            </div>
+            <img src="../../../public/img/accessibility.png" alt="" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Footer
+export default Footer;
