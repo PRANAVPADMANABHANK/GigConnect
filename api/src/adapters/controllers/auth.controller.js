d0c7e4504697388146593e57c1b1391143587d9b
pranav k <pranavkkeloth@gmail.com>
2023-07-26 18:49:13 +0530

    chore: Update jwt and cookie-parser

--- api/src/adapters/controllers/auth.controller.js
+++ api/src/adapters/controllers/auth.controller.js
@@ -1,27 +1,34 @@
-import User from "../../core/entities/user.model.js"
+import User from "../../core/entities/user.model.js";
+import bcrypt from "bcrypt";
 
-export const register = async(req,res) => {
-    try {
-        const newUser = new User(req.body)
-        await newUser.save()
-        res.status(201).send("User has been created.")
-    } catch (error) {
-        res.status(500).send("Something went wrong!")
-    }
-}
+export const register = async (req, res) => {
+  try {
+    const hash = bcrypt.hashSync(req.body.password, 5);
+    const newUser = new User({
+      ...req.body,
+      password: hash,
+    });
+    await newUser.save();
+    res.status(201).send("User has been created.");
+  } catch (error) {
+    res.status(500).send("Something went wrong!");
+  }
+};
 
-export const login = async(req,res) => {
-    try {
-        
-    } catch (error) {
-        
-    }
-}
+export const login = async (req, res) => {
+  try {
+    const user = await User.findOne({ username: req.body.username });
+    if (!user) return res.status(404).send("User not found!");
 
-export const logout = async(req,res) => {
-    try {
-        
-    } catch (error) {
-        
-    }
-}
+    const isCorrect = bcrypt.compareSync(req.body.password, user.password);
+    if (!isCorrect) return res.status(400).send("Wrong password or username!");
+
+    const { password, ...info } = user._doc;
+    res.status(200).send(info);
+  } catch (error) {}
+};
+
+export const logout = async (req, res) => {
+  try {
+  } catch (error) {}
+};
