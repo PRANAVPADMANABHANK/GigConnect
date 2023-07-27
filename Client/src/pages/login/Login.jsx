ea4d296a805aafbd1aabb3f420b2770785f8596f
pranav k <pranavkkeloth@gmail.com>
2023-07-27 18:12:29 +0530

    chore: Update axios

--- Client/src/pages/login/Login.jsx
+++ Client/src/pages/login/Login.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import React from "react";
+import { useState } from "react";
+import "./Login.scss";
 
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
 
-export default Login
+  const handleSubmit = (e) => {
+    e.preventDefault(); //to prevent the refreshing of the page
+    
+  };
+
+  return (
+    <div className="login">
+      <form onSubmit={handleSubmit}>
+        <h1>Sign in</h1>
+        <label htmlFor="">Username</label>
+        <input
+          name="username"
+          type="text"
+          placeholder="johndoe"
+          onChange={(e) => setUsername(e.target.value)}
+        />
+
+        <label htmlFor="">Password</label>
+        <input
+          name="password"
+          type="password"
+          placeholder="johndoe@gmail.com"
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
