751a05c3ac9b8f039c0f019c6ce6763f5377f5d7
pranav k <pranavkkeloth@gmail.com>
2023-07-30 11:12:50 +0530

    chore: Update @tanstack/react-query

--- api/src/adapters/controllers/gig.controller.js
+++ api/src/adapters/controllers/gig.controller.js
@@ -12,7 +12,7 @@ export const createGig = async (req, res, next) => {
   });
   try {
     const savedGig = await newGig.save();
-    res.send(201).json(savedGig);
+    res.status(201).json(savedGig);
   } catch (err) {
     next(err);
   }
@@ -20,17 +20,43 @@ export const createGig = async (req, res, next) => {
 
 export const deleteGig = async (req, res, next) => {
   try {
+    const gig = await Gig.findById(req.params.id); // "req.params.id" is the gig id
+
+    if (gig.userId !== req.userId)
+      return next(createError(403, "You can delete only your gig!"));
+
+    await Gig.findByIdAndDelete(req.params.id);
+    res.status(200).send("Gig has been deleted!");
   } catch (err) {
-    console.log(err);
+    next(err);
   }
 };
 
 export const getGig = async (req, res, next) => {
   try {
-  } catch (err) {}
+    const gig = await Gig.findById(req.params.id);
+    if (!gig) next(createError(404, "Gig not found!"));
+    res.status(200).send(gig);
+  } catch (err) {
+    next(err);
+  }
 };
 
+// filtering using query params
 export const getGigs = async (req, res, next) => {
+  const q = req.query; // example of this output({ cat: 'design', search: 'g' })
+  const filters = {
+    ...(q.uerId && { userId: q.userId }), // If the q.userId exists (not undefined or empty), it adds a property userId with the value of q.userId to the filters object
+    ...(q.cat && { cat: q.cat }), //  If the q.cat exists (not undefined or empty), it adds a property cat with the value of q.cat to the filters object
+    ...((q.min || q.max) && {
+      price: { ...(q.min && { $gt: q.min }), ...(q.max && { $lt: q.max }) },
+    }), //  If either q.min or q.max exists (not undefined or empty), it creates a nested price object with the properties
+    ...(q.search && { title: { $regex: q.search, $options: "i" } }), // $options:"i" => gives the lower or uppercase search results
+  };
   try {
-  } catch (err) {}
+    const gigs = await Gig.find(filters);
+    res.status(200).send(gigs);
+  } catch (err) {
+    next(err);
+  }
 };
