ee7f73cc581a9fe72bae513678f1ba47ef764841
pranav k <pranavkkeloth@gmail.com>
2023-08-01 18:50:02 +0530

    feat: Implement buyer reviews and ratings with back end validation

--- Client/src/components/reviews/Reviews.jsx
+++ Client/src/components/reviews/Reviews.jsx
@@ -1,10 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import Review from "../review/Review";
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import newRequest from "../../utils/newRequest";
 import "./Reviews.scss";
 
 const Reviews = ({ gigId }) => {
+  const [err, setErr] = useState(null);
 
   // used for updating the review of each user given
   const queryClient = useQueryClient();
@@ -25,29 +26,34 @@ const Reviews = ({ gigId }) => {
     },
     onSuccess: () => {
       queryClient.invalidateQueries(["reviews"]) // used to update the review on the review page
-    }
-  })
+    },
+    onError: (error) => {
+      setErr(error.response.data); // Store the error message in the state to display it
+    },
+  });
 
   const handleSubmit = (e) => {
     e.preventDefault();
     const desc = e.target[0].value; // fetch desc from input
     const star = e.target[1].value; // fetch stars number from select
-    mutation.mutate({ gigId, desc, star })
-  }
+    mutation.mutate({ gigId, desc, star }); // pass these values to the mutation function
+  };
+
   return (
     <div className="reviews">
       <h2>Reviews</h2>
       {isLoading ? "Loading..." : error ? "Something went wrong" : data.map((review) => <Review key={review._id} review={review} />)}
       <div className="add">
         <h3>Add a review</h3>
-        <form action="" onSubmit={handleSubmit}>
+        <form action="" className="addForm" onSubmit={handleSubmit}>
           <input type="text" placeholder="Write your opinion" />
+          <span>{err === "Review validation failed: desc: Path `desc` is required."?"Please fill the required field": err}</span> {/* Display the error message inside the span */}
           <select name="" id="">
-            <option value={1}>1</option>
-            <option value={2}>2</option>
-            <option value={3}>3</option>
-            <option value={4}>4</option>
-            <option value={5}>5</option>
+            <option value={1}>1⭐</option>
+            <option value={2}>2⭐</option>
+            <option value={3}>3⭐</option>
+            <option value={4}>4⭐</option>
+            <option value={5}>5⭐</option>
           </select>
           <button>Send</button>
         </form>
