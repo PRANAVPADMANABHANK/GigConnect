3872708135aa361a09ffed9acd2de55b982b2ea6
pranav k <pranavkkeloth@gmail.com>
2023-09-01 19:34:05 +0530

    feat: Implement adminNavbar

--- Client/src/components/adminComponents/adminNavbar/AdminNavbar.jsx
+++ Client/src/components/adminComponents/adminNavbar/AdminNavbar.jsx
@@ -1,239 +1,3 @@
-// import * as React from 'react';
-// import { styled, alpha } from '@mui/material/styles';
-// import AppBar from '@mui/material/AppBar';
-// import Box from '@mui/material/Box';
-// import Toolbar from '@mui/material/Toolbar';
-// import IconButton from '@mui/material/IconButton';
-// import Typography from '@mui/material/Typography';
-// import InputBase from '@mui/material/InputBase';
-// import Badge from '@mui/material/Badge';
-// import MenuItem from '@mui/material/MenuItem';
-// import Menu from '@mui/material/Menu';
-// import MenuIcon from '@mui/icons-material/Menu';
-// import SearchIcon from '@mui/icons-material/Search';
-// import AccountCircle from '@mui/icons-material/AccountCircle';
-// import MailIcon from '@mui/icons-material/Mail';
-// import NotificationsIcon from '@mui/icons-material/Notifications';
-// import MoreIcon from '@mui/icons-material/MoreVert';
-
-// const Search = styled('div')(({ theme }) => ({
-//   position: 'relative',
-//   borderRadius: theme.shape.borderRadius,
-//   backgroundColor: alpha(theme.palette.common.white, 0.15),
-//   '&:hover': {
-//     backgroundColor: alpha(theme.palette.common.white, 0.25),
-//   },
-//   marginRight: theme.spacing(2),
-//   marginLeft: 0,
-//   width: '100%',
-//   [theme.breakpoints.up('sm')]: {
-//     marginLeft: theme.spacing(3),
-//     width: 'auto',
-//   },
-// }));
-
-// const SearchIconWrapper = styled('div')(({ theme }) => ({
-//   padding: theme.spacing(0, 2),
-//   height: '100%',
-//   position: 'absolute',
-//   pointerEvents: 'none',
-//   display: 'flex',
-//   alignItems: 'center',
-//   justifyContent: 'center',
-// }));
-
-// const StyledInputBase = styled(InputBase)(({ theme }) => ({
-//   color: 'inherit',
-//   '& .MuiInputBase-input': {
-//     padding: theme.spacing(1, 1, 1, 0),
-//     // vertical padding + font size from searchIcon
-//     paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-//     transition: theme.transitions.create('width'),
-//     width: '100%',
-//     [theme.breakpoints.up('md')]: {
-//       width: '20ch',
-//     },
-//   },
-// }));
-
-// export default function PrimarySearchAppBar() {
-//   const [anchorEl, setAnchorEl] = React.useState(null);
-//   const [mobileMoreAnchorEl, setMobileMoreAnchorEl] = React.useState(null);
-
-//   const isMenuOpen = Boolean(anchorEl);
-//   const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);
-
-//   const handleProfileMenuOpen = (event) => {
-//     setAnchorEl(event.currentTarget);
-//   };
-
-//   const handleMobileMenuClose = () => {
-//     setMobileMoreAnchorEl(null);
-//   };
-
-//   const handleMenuClose = () => {
-//     setAnchorEl(null);
-//     handleMobileMenuClose();
-//   };
-
-//   const handleMobileMenuOpen = (event) => {
-//     setMobileMoreAnchorEl(event.currentTarget);
-//   };
-
-//   const menuId = 'primary-search-account-menu';
-//   const renderMenu = (
-//     <Menu
-//       anchorEl={anchorEl}
-//       anchorOrigin={{
-//         vertical: 'top',
-//         horizontal: 'right',
-//       }}
-//       id={menuId}
-//       keepMounted
-//       transformOrigin={{
-//         vertical: 'top',
-//         horizontal: 'right',
-//       }}
-//       open={isMenuOpen}
-//       onClose={handleMenuClose}
-//     >
-//       <MenuItem onClick={handleMenuClose}>Profile</MenuItem>
-//       <MenuItem onClick={handleMenuClose}>My account</MenuItem>
-//     </Menu>
-//   );
-
-//   const mobileMenuId = 'primary-search-account-menu-mobile';
-//   const renderMobileMenu = (
-//     <Menu
-//       anchorEl={mobileMoreAnchorEl}
-//       anchorOrigin={{
-//         vertical: 'top',
-//         horizontal: 'right',
-//       }}
-//       id={mobileMenuId}
-//       keepMounted
-//       transformOrigin={{
-//         vertical: 'top',
-//         horizontal: 'right',
-//       }}
-//       open={isMobileMenuOpen}
-//       onClose={handleMobileMenuClose}
-//     >
-//       <MenuItem>
-//         <IconButton size="large" aria-label="show 4 new mails" color="inherit">
-//           <Badge badgeContent={4} color="error">
-//             <MailIcon />
-//           </Badge>
-//         </IconButton>
-//         <p>Messages</p>
-//       </MenuItem>
-//       <MenuItem>
-//         <IconButton
-//           size="large"
-//           aria-label="show 17 new notifications"
-//           color="inherit"
-//         >
-//           <Badge badgeContent={17} color="error">
-//             <NotificationsIcon />
-//           </Badge>
-//         </IconButton>
-//         <p>Notifications</p>
-//       </MenuItem>
-//       <MenuItem onClick={handleProfileMenuOpen}>
-//         <IconButton
-//           size="large"
-//           aria-label="account of current user"
-//           aria-controls="primary-search-account-menu"
-//           aria-haspopup="true"
-//           color="inherit"
-//         >
-//           <AccountCircle />
-//         </IconButton>
-//         <p>Profile</p>
-//       </MenuItem>
-//     </Menu>
-//   );
-
-//   return (
-//     <Box sx={{ flexGrow: 1 }}>
-//       <AppBar position="static">
-//         <Toolbar>
-//           <IconButton
-//             size="large"
-//             edge="start"
-//             color="inherit"
-//             aria-label="open drawer"
-//             sx={{ mr: 2 }}
-//           >
-//             <MenuIcon />
-//           </IconButton>
-//           <Typography
-//             variant="h6"
-//             noWrap
-//             component="div"
-//             sx={{ display: { xs: 'none', sm: 'block' } }}
-//           >
-//             MUI
-//           </Typography>
-//           <Search>
-//             <SearchIconWrapper>
-//               <SearchIcon />
-//             </SearchIconWrapper>
-//             <StyledInputBase
-//               placeholder="Search…"
-//               inputProps={{ 'aria-label': 'search' }}
-//             />
-//           </Search>
-//           <Box sx={{ flexGrow: 1 }} />
-//           <Box sx={{ display: { xs: 'none', md: 'flex' } }}>
-//             <IconButton size="large" aria-label="show 4 new mails" color="inherit">
-//               <Badge badgeContent={4} color="error">
-//                 <MailIcon />
-//               </Badge>
-//             </IconButton>
-//             <IconButton
-//               size="large"
-//               aria-label="show 17 new notifications"
-//               color="inherit"
-//             >
-//               <Badge badgeContent={17} color="error">
-//                 <NotificationsIcon />
-//               </Badge>
-//             </IconButton>
-//             <IconButton
-//               size="large"
-//               edge="end"
-//               aria-label="account of current user"
-//               aria-controls={menuId}
-//               aria-haspopup="true"
-//               onClick={handleProfileMenuOpen}
-//               color="inherit"
-//             >
-//               <AccountCircle />
-//             </IconButton>
-//           </Box>
-//           <Box sx={{ display: { xs: 'flex', md: 'none' } }}>
-//             <IconButton
-//               size="large"
-//               aria-label="show more"
-//               aria-controls={mobileMenuId}
-//               aria-haspopup="true"
-//               onClick={handleMobileMenuOpen}
-//               color="inherit"
-//             >
-//               <MoreIcon />
-//             </IconButton>
-//           </Box>
-//         </Toolbar>
-//       </AppBar>
-//       {renderMobileMenu}
-//       {renderMenu}
-//     </Box>
-//   );
-// }
-
-
-
 import React, { useState } from "react";
 import {
   AppBar,
@@ -253,7 +17,12 @@ import DashboardIcon from "@mui/icons-material/Dashboard";
 import PersonIcon from "@mui/icons-material/Person";
 import NotificationsIcon from "@mui/icons-material/Notifications";
 import AccountCircleIcon from "@mui/icons-material/AccountCircle";
-import { BrowserRouter as Router, Route, Link, useNavigate } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  useNavigate,
+} from "react-router-dom";
 import newRequest from "../../../utils/newRequest";
 
 const Dashboard = () => <h2>Dashboard</h2>;
@@ -263,12 +32,11 @@ const App = () => {
   const [open, setOpen] = useState(false);
   const [selected, setSelected] = useState("dashboard");
   const [profileMenuAnchor, setProfileMenuAnchor] = useState(null);
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
-  const currentAdmin = JSON.parse(localStorage.getItem("currentAdmin"))
+  const currentAdmin = JSON.parse(localStorage.getItem("currentAdmin"));
 
-  console.log(currentAdmin, "currentAdmin")
-  
+  console.log(currentAdmin, "currentAdmin");
 
   const handleListItemClick = (item) => {
     setSelected(item);
@@ -283,7 +51,7 @@ const App = () => {
     setProfileMenuAnchor(event.currentTarget);
   };
 
-  const handleProfileMenuClose = async() => {
+  const handleProfileMenuClose = async () => {
     setProfileMenuAnchor(null);
     try {
       await newRequest.post("admin/logout");
@@ -297,42 +65,52 @@ const App = () => {
   return (
     <div style={{ display: "flex" }}>
       <Drawer variant="temporary" open={open} onClose={() => setOpen(false)}>
-        {currentAdmin && <List>
-          <ListItem
-            button
-            component={Link}
-            to="/admin/dashboard"
-            onClick={() => handleListItemClick("dashboard")}
-            selected={selected === "dashboard"}
-          >
-            <ListItemIcon style={{
-                color: selected === "dashboard" ? "blue" : "inherit",
-              }}>
-              <DashboardIcon />
-            </ListItemIcon>
-            <ListItemText primary="Dashboard"
-              style={{
-                color: selected === "dashboard" ? "blue" : "inherit",
-              }} />
-          </ListItem>
-          <ListItem
-            button
-            component={Link}
-            to="/admin/users"
-            onClick={() => handleListItemClick("profile")}
-            selected={selected === "profile"}
-          >
-            <ListItemIcon style={{
-                color: selected === "profile" ? "blue" : "inherit",
-              }}>
-              <PersonIcon />
-            </ListItemIcon>
-            <ListItemText primary="Users"
-              style={{
-                color: selected === "profile" ? "blue" : "inherit",
-              }} />
-          </ListItem>
-        </List>}
+        {currentAdmin && (
+          <List>
+            <ListItem
+              button
+              component={Link}
+              to="/admin/dashboard"
+              onClick={() => handleListItemClick("dashboard")}
+              selected={selected === "dashboard"}
+            >
+              <ListItemIcon
+                style={{
+                  color: selected === "dashboard" ? "blue" : "inherit",
+                }}
+              >
+                <DashboardIcon />
+              </ListItemIcon>
+              <ListItemText
+                primary="Dashboard"
+                style={{
+                  color: selected === "dashboard" ? "blue" : "inherit",
+                }}
+              />
+            </ListItem>
+            <ListItem
+              button
+              component={Link}
+              to="/admin/users"
+              onClick={() => handleListItemClick("profile")}
+              selected={selected === "profile"}
+            >
+              <ListItemIcon
+                style={{
+                  color: selected === "profile" ? "blue" : "inherit",
+                }}
+              >
+                <PersonIcon />
+              </ListItemIcon>
+              <ListItemText
+                primary="Users"
+                style={{
+                  color: selected === "profile" ? "blue" : "inherit",
+                }}
+              />
+            </ListItem>
+          </List>
+        )}
       </Drawer>
       <div style={{ flexGrow: 1, padding: "20px" }}>
         <AppBar position="static">
@@ -357,7 +135,8 @@ const App = () => {
             </IconButton> */}
             <IconButton color="inherit" onClick={handleProfileMenuOpen}>
               <AccountCircleIcon />
-            </IconButton>{currentAdmin ? currentAdmin?.username : ""}
+            </IconButton>
+            {currentAdmin ? currentAdmin?.username : ""}
             <Menu
               id="profile-menu"
               anchorEl={profileMenuAnchor}
@@ -366,7 +145,11 @@ const App = () => {
             >
               {/* <MenuItem onClick={handleProfileMenuClose}>Profile</MenuItem>
               <MenuItem onClick={handleProfileMenuClose}>My account</MenuItem> */}
-              <MenuItem onClick={handleProfileMenuClose}>Logout</MenuItem>
+              {currentAdmin ? (
+                <MenuItem onClick={handleProfileMenuClose}>Logout</MenuItem>
+              ) : (
+                <MenuItem to="/admin/register">Login</MenuItem>
+              )}
             </Menu>
           </Toolbar>
         </AppBar>
@@ -381,5 +164,3 @@ const App = () => {
 };
 
 export default App;
-
-
