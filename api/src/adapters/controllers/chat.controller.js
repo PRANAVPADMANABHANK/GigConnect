259c4ecb782ecd37e45b44be795a1898d167fb28
pranav k <pranavkkeloth@gmail.com>
2023-09-05 11:23:04 +0530

    feat: Implement addGroup & removeGroup backend

--- api/src/adapters/controllers/chat.controller.js
+++ api/src/adapters/controllers/chat.controller.js
@@ -9,7 +9,6 @@ import User from "../../core/entities/user.model.js";
 export const accessChat = async (req, res) => {
   const { userId } = req.body;
 
-
   if (!userId) {
     console.log("UserId param not sent with request");
     return res.sendStatus(400);
@@ -110,5 +109,87 @@ export const createGroupChat = async (req, res) => {
   } catch (error) {
     res.status(400);
     throw new Error(error.message);
+  }
+};
+
+// @desc    Rename Group
+// @route   PUT /api/chat/rename
+// @access  verifyToken
+export const renameGroup = async (req, res) => {
+  const { chatId, chatName } = req.body;
+
+  const updatedChat = await Chat.findByIdAndUpdate(
+    chatId,
+    {
+      chatName: chatName,
+    },
+    {
+      new: true,
+    }
+  )
+    .populate("users", "-password")
+    .populate("groupAdmin", "-password");
+
+  if (!updatedChat) {
+    res.status(404);
+    throw new Error("Chat Not Found");
+  } else {
+    res.json(updatedChat);
+  }
+};
+
+// @desc    Add user to Group / Leave
+// @route   PUT /api/chat/groupadd
+// @access  verifyToken
+export const addToGroup = async (req, res) => {
+  const { chatId, userId } = req.body;
+
+  // check if the requester is admin
+
+  const added = await Chat.findByIdAndUpdate(
+    chatId,
+    {
+      $push: { users: userId },
+    },
+    {
+      new: true,
+    }
+  )
+    .populate("users", "-password")
+    .populate("groupAdmin", "-password");
+
+  if (!added) {
+    res.status(404);
+    throw new Error("Chat Not Found");
+  } else {
+    res.json(added);
+  }
+};
+
+// @desc    Remove user from Group
+// @route   PUT /api/chat/groupremove
+// @access  verifyToken
+export const removeFromGroup = async (req, res) => {
+  const { chatId, userId } = req.body;
+
+  // check if the requester is admin
+
+  const removed = await Chat.findByIdAndUpdate(
+    chatId,
+    {
+      $pull: { users: userId },
+    },
+    {
+      new: true,
+    }
+  )
+    .populate("users", "-password")
+    .populate("groupAdmin", "-password");
+
+  if (!removed) {
+    res.status(404);
+    throw new Error("Chat Not Found");
+  } else {
+    res.json(removed);
   }
 };
--- api/src/adapters/routes/chat.route.js
+++ api/src/adapters/routes/chat.route.js
@@ -1,14 +1,14 @@
 import express from "express";
 import { verifyToken } from "../../interfaces/gateways/jwt.js";
-import { accessChat, fetchChats, createGroupChat } from "../controllers/chat.controller.js";
+import { accessChat, fetchChats, createGroupChat, renameGroup, addToGroup, removeFromGroup } from "../controllers/chat.controller.js";
 
 const router = express.Router();
 
 router.post("/", verifyToken, accessChat)
 router.get("/",verifyToken, fetchChats);
 router.post("/group", verifyToken, createGroupChat);
-// router.put("/rename", renameGroup);
-// router.put("/groupremove", removeFromGroup);
-// router.put("/groupadd", addToGroup);
+router.put("/rename",verifyToken, renameGroup);
+router.put("/groupadd",verifyToken, addToGroup); 
+router.put("/groupremove",verifyToken, removeFromGroup);
 
 export default router;
