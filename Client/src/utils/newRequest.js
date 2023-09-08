27da5aa24b73e1ef399f943ab6e2b8482eb14d3c
pranav k <pranavkkeloth@gmail.com>
2023-09-08 18:18:54 +0530

    feat: bug fix 2

--- Client/src/utils/newRequest.js
+++ Client/src/utils/newRequest.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const newRequest = axios.create({
-  baseURL: "http://localhost:8800/api/",
+  baseURL: "http://gigconnect.stepups.live/api/",
   withCredentials: true,
 });
 
