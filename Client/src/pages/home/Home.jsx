0e0d3945b6b07e8e9149a89d18ccd5740d6e463b
pranav k <pranavkkeloth@gmail.com>
2023-09-08 17:40:06 +0530

    feat: bug fixed

--- Client/src/pages/home/Home.jsx
+++ Client/src/pages/home/Home.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Featured from "../../components/featured/Featured";
-import TrustedBy from "../../components/trustedBy/trustedBy";
+import TrustedBy from "../../components/trustedBy/TrustedBy";
 import Slide from "../../components/slide/Slide";
 import { cards, projects } from "../../data";
 import CatCard from "../../components/catCard/CatCard";
