259ec26d50f4aa4387540c678a2b9e1305ef69e4
pranav k <pranavkkeloth@gmail.com>
2023-09-17 21:52:05 +0530

    feat: img updated

--- Client/src/utils/newRequest.js
+++ Client/src/utils/newRequest.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const newRequest = axios.create({
-  baseURL: "https://gigconnect.stepups.live/api/",
+  baseURL: "http://localhost:8800/api/",
   withCredentials: true,
 });
 
