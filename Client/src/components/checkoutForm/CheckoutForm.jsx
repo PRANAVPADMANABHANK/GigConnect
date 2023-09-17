093aef0fe2c8c06e0d216534f6872365093a0217
pranav k <pranavkkeloth@gmail.com>
2023-09-17 20:08:52 +0530

    feat: added https

--- Client/src/components/checkoutForm/CheckoutForm.jsx
+++ Client/src/components/checkoutForm/CheckoutForm.jsx
@@ -61,7 +61,7 @@ const CheckoutForm = () => {
       elements,
       confirmParams: {
         // Make sure to change this to your payment completion page
-        return_url: "http://gigconnect.stepups.live/success",
+        return_url: "https://gigconnect.stepups.live/success",
       },
     });
     console.log(error,"llllllll")
