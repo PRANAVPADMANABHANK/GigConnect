cf2feb7071ea131c82b38daca53fa44a8110aec4
pranav k <pranavkkeloth@gmail.com>
2023-09-17 19:40:57 +0530

    feat: bug fixed

--- Client/src/components/checkoutForm/CheckoutForm.jsx
+++ Client/src/components/checkoutForm/CheckoutForm.jsx
@@ -61,7 +61,7 @@ const CheckoutForm = () => {
       elements,
       confirmParams: {
         // Make sure to change this to your payment completion page
-        return_url: "http://gigconnect.stepups.live/success",
+        return_url: "http://localhost:5173/success",
       },
     });
     console.log(error,"llllllll")
